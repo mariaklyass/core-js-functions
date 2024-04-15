483d15a995b636dcd7441792055577fff9926eb6
mariaklyass <klyassmaria@gmail.com>
2024-04-15 23:30:57 +0300

    feat: solve logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -179,8 +179,17 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function namedLogger(...args) {
+    const argsString = args.map((arg) => JSON.stringify(arg)).join(',');
+    const functionName = func.name || 'anonymous';
+
+    logFunc(`${functionName}(${argsString}) starts`);
+    const result = func(...args);
+    logFunc(`${functionName}(${argsString}) ends`);
+
+    return result;
+  };
 }
 
 /**
